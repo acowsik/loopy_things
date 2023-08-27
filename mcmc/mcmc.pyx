91f8e31613fe1394063d4b0ceb3fae16da1cd0c7
acowsik <aditya.cowsik@gmail.com>
2023-08-27 13:59:47 -0700

    Extracted the random lists into their own classes

--- mcmc/mcmc.pyx
+++ mcmc/mcmc.pyx
@@ -6,18 +6,82 @@ import numpy as np
 cimport numpy as np
 from math import exp
 
+def is_power_of_2(n):
+    while n > 1:
+        if n & 1:
+            return False
+        
+        n = n >> 1
+    
+    return True
+
+    
+cdef class RandomLongs:
+    cdef public long size
+    cdef long[:] array
+    cdef long rand_index
+    cdef public long maximum
+
+    def __init__(self, long size, long maximum):
+        assert is_power_of_2(size)
+        self.maximum = maximum
+        self.size = size
+        self.rand_index = 0
+        self.refresh()
+
+    def refresh(self):
+        self.array = np.random.randint(0, self.maximum, size=self.size)
+    
+    cpdef long next(self):
+        cdef long out = self.array[self.rand_index]
+        self.rand_index += 1
+
+        # refresh the random numbers if needed
+        # works because rand_size is a power of 2
+        if self.rand_index & self.size: 
+            self.refresh()
+            self.rand_index = 0
+        
+        return out
+
+cdef class RandomDoubles:
+    cdef public long size
+    cdef double[:] array
+    cdef long rand_index
+    def __init__(self, long size):
+        assert is_power_of_2(size)
+        self.size = size
+        self.rand_index = 0
+        self.refresh()
+
+    def refresh(self):
+        self.array = np.random.random(size=self.size)
+    
+    cpdef double next(self):
+        cdef double out = self.array[self.rand_index]
+        self.rand_index += 1
+
+        # refresh the random numbers if needed
+        # works because rand_size is a power of 2
+        if self.rand_index & self.size: 
+            self.refresh()
+            self.rand_index = 0
+        
+        return out
+
+
 cdef class Ising:
     cdef public char[:] state
     cdef public long x_size
     cdef public long y_size
     cdef public long N
     cdef unsigned long Nm1
-    cdef long long rand_index
-    cdef long rand_size
-    cdef double[:] random_nums
-    cdef long[:] random_spins
+    cdef RandomDoubles random_nums
+    cdef RandomLongs random_spins
     cdef double beta
     cdef double [:]precomputed_probabilities
+    cdef long long spin_marker_value
+    cdef long long spin_marks[:]
 
     def __init__(self, log_x_size, log_y_size, temperature):
         """
@@ -33,31 +97,26 @@ cdef class Ising:
         self.beta = 1.0 / temperature
 
         # generate a bunch of random numbers initially, I have found this is more 
-        # efficient somehow
-        self.rand_index = 0
-        self.rand_size = 16 * self.N
-        self.random_spins = np.random.randint(0, self.N, size=self.rand_size)
-        self.random_nums = np.random.random(size=self.rand_size)
+        # efficient
+        self.random_spins = RandomLongs(16 * self.N, self.N)
+        self.random_nums = RandomDoubles(16 * self.N)
 
         # precompute the exponentials to make things faster
         # about 4.5 times!
         self.precomputed_probabilities = np.exp(-2 * self.beta * (np.arange(9) - 4))
+
+        # initialize the marker for the spins so that we don't have to backtrack 
+        # and unmark spins
+        self.spin_marker_value = 1
+        self.spin_marks = np.zeros(self.N, dtype=np.int64)
         
 
     cpdef void step(self):
         """
         does one potential spin flip 
         """
-        cdef unsigned long spin = self.random_spins[self.rand_index]
-        cdef double rand_val = self.random_nums[self.rand_index]
-        self.rand_index += 1
-
-        # refresh the random numbers if needed
-        # works because rand_size is a power of 2
-        if self.rand_index & self.rand_size: 
-            self.random_spins = np.random.randint(0, self.N, size=self.rand_size)
-            self.random_nums = np.random.random(size=self.rand_size)
-            self.rand_index = 0
+        cdef unsigned long spin = self.random_spins.next()
+        cdef double rand_val = self.random_nums.next()
         
         # find the net spin of the neighbors
         # the "& self.Nm1" is equivalent to going modulo self.N
@@ -77,6 +136,11 @@ cdef class Ising:
         cdef int i
         for i in range(self.N):
             self.step()
+
+    cpdef void wolff_sweep(self):
+        cdef long spin = self.random_spins.next()
+        
+
 
 
     cpdef long magnetization(self):
